a77a2dcbe4aa53543b0b85c6f4d31474750d219c
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-08-25 11:57:01 +0530

    Changed volume of ball to radius.

--- constants.py
+++ constants.py
@@ -3,9 +3,7 @@
 ### Constants ###
 g = 9.8  # m/s^2
 COEFFICIENT_OF_VISCOSITY = 1.81e-5  # Pa.s
-DENSITY_OF_AIR = 1.204  # Kg/m^2
+DENSITY_OF_AIR = 1.204  # Kg/m^3
 DENSITY_OF_BALL = 811.11  # Kg/m^3
-VOLUME_OF_BALL = 1.8e-4  # m^3
+RADIUS_OF_BALL = 3.556e-2  # cm
 #################
-
-
