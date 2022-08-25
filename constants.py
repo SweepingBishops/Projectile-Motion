0945b43b1a1eb8492695afbe93ed4c78028477cb
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-08-25 11:51:09 +0530

    Added constants file.

---
+++ constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+### Constants ###
+g = 9.8  # m/s^2
+COEFFICIENT_OF_VISCOSITY = 1.81e-5  # Pa.s
+DENSITY_OF_AIR = 1.204  # Kg/m^2
+DENSITY_OF_BALL = 811.11  # Kg/m^3
+VOLUME_OF_BALL = 1.8e-4  # m^3
+#################
+
+
