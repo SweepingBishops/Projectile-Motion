cd48cb57bcc8f5a516b5f6c3caa3b8bc941e69d6
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-08-28 16:28:01 +0530

    Revert "Changed constants."
    This reverts commit 193948533a4fcf74a3b5521caa2bbdb42ccbf0f9.

--- constants.py
+++ constants.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 ### Constants ###
-ACCELERATION_DUE_TO_GRAVITY = 9.8  # m/s^2
-COEFFICIENT_OF_DRAG = 1 
+g = 9.8  # m/s^2
+COEFFICIENT_OF_VISCOSITY = 1.81e-5  # Pa.s
+DENSITY_OF_AIR = 1.204  # Kg/m^3
+DENSITY_OF_BALL = 811.11  # Kg/m^3
+RADIUS_OF_BALL = 3.556e-2  # cm
 #################
