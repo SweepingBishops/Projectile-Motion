d3cd3e36b960120c3de3fca454dabf63f2f2cbd7
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-08-28 22:43:05 +0530

    Completed? Phew... Have to check.

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 import math
 import numpy as np
-#from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 ### Constants ###
-g = -10  # m/s^2
+g = -9.8  # m/s^2
 m = 0.150  # Kg
-μ = 0
+μ = 0.003
+TIME_STEP = 0.00001
+INIT_VEL = [4, 3]
+INIT_POS = [0, 0]
 #################
-class vector():
+class vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.mag = math.sqrt(self.x**2 + self.y**2) 
+        self.mag = math.sqrt(self.x**2 + self.y**2)
+
     def __add__(self, other):
         return vector(self.x + other.x, self.y + other.y)
 
@@ -21,45 +24,66 @@ class vector():
         return vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, scalar):
-        return vector(self.x*scalar, self.y*scalar)
-    
+        return vector(self.x * scalar, self.y * scalar)
+
     def __truediv__(self, scalar):
-        return vector(self.x/scalar, self.y/scalar)
+        return vector(self.x / scalar, self.y / scalar)
 
 
-class ball():
-    def __init__(self, init_vel, init_pos=[0,0]):
+class object:
+    def __init__(self, init_vel, init_pos=[0, 0], friction=True):
         self.pos = vector(init_pos[0], init_pos[1])
         self.vel = vector(init_vel[0], init_vel[1])
+        self.friction = friction
 
     def force(self):
-        return self.drag() + vector(0,g)
+        if self.friction:
+            return self.drag() + vector(0, m * g)
+        else:
+            return vector(0, m * g)
 
     def drag(self):
-        drag_mag = μ*self.vel.mag**2
-        drag_x = -1*drag_mag * (self.vel.x / self.vel.mag)
-        drag_y = -1*drag_mag * (self.vel.y / self.vel.mag)
+        drag_mag = μ * (self.vel.mag**2)
+        drag_x = -1 * drag_mag * (self.vel.x / self.vel.mag)
+        drag_y = -1 * drag_mag * (self.vel.y / self.vel.mag)
         return vector(drag_x, drag_y)
-         
+
     def accel(self):
-        return self.force()/m
+        return self.force() / m
 
     def move(self, time):
-        self.pos += self.vel
+        self.pos += self.vel * time
         self.vel += self.accel() * time
 
-ball1 = ball([5,5]) 
-xpos = []
-ypos = []
-for i in range(50):
-    xpos.append(ball1.pos.x)
-    ypos.append(ball1.pos.y)
-    ball1.move(0.0028)
 
-print(xpos,ypos ,sep="\n"*3)
-print("H = ",max(ypos))
+def get_points(init_vel, init_pos=[0, 0]):
+    ball = object(init_vel, init_pos)
+    xpos = []
+    ypos = []
+    xpos.append(ball.pos.x)
+    ypos.append(ball.pos.y)
+    ball.move(TIME_STEP)
+    while ball.pos.y >= 0:
+        xpos.append(ball.pos.x)
+        ypos.append(ball.pos.y)
+        ball.move(TIME_STEP)
 
-plt.xlabel("x")
-plt.ylabel("y")
-plt.plot(xpos, ypos)
+    ball1 = object(init_vel, init_pos, friction=False)
+    xpos1 = []
+    ypos1 = []
+    xpos1.append(ball1.pos.x)
+    ypos1.append(ball1.pos.y)
+    ball1.move(TIME_STEP)
+    while ball1.pos.y >= 0:
+        xpos1.append(ball1.pos.x)
+        ypos1.append(ball1.pos.y)
+        ball1.move(TIME_STEP)
+    return xpos, ypos, xpos1, ypos1
+
+
+xpos, ypos, xpos1, ypos1 = get_points(INIT_VEL, INIT_POS)
+
+plt.xlabel("x -->")
+plt.ylabel("y -->")
+plt.plot(xpos, ypos, ",-k", xpos1, ypos1, ",--k")
 plt.show()
